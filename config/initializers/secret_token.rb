405aed03bcb6d405db0c39da481e9517331da1b6
mike <mike@mightybell.com>
2013-04-06 17:12:30 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mldb::Application.config.secret_token = '5710ea93747697a8fd5987ac256c0d6f234ed38b242e09caec05839d9d6d477a666bf0c406c9fbf107e42cb16b0aaa1866df62d515264756e749b878b10bd68d'
